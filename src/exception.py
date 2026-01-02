0e0ee143543cb42b204aa44cda5d8233af30f6ad
Tushar9184 <sabharwaltushar02@gmail.com>
2026-01-02 12:26:20 +0530

    model trainer

--- src/exception.py
+++ src/exception.py
@@ -4,7 +4,7 @@ from src.logger import logging
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    # Added a missing ']' after {1} below
+   
     error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
@@ -12,7 +12,7 @@ def error_message_detail(error, error_detail: sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message) # Fixed the super() call
+        super().__init__(error_message) 
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
     
     def __str__(self):
