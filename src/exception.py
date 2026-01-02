49b3b8cbf596281a4bcd4fcd24b9ca09cf95ce1c
Tushar9184 <sabharwaltushar02@gmail.com>
2026-01-03 00:20:07 +0530

    app.py

--- src/exception.py
+++ src/exception.py
@@ -7,7 +7,7 @@ def error_message_detail(error, error_detail: sys):
    
     error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
-    )
+    ) 
     return error_message
 
 class CustomException(Exception):
